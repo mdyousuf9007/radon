cc123686bb192b692d9425e1d1ccff6a34993e36
mdyousuf <yousuf9007@gmail.com>
2022-06-10 00:32:13 +0530

    complete

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,31 @@
 const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const publisherModel=require("../models/publisherModel")
 
+//Q.3
 const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
+    let bookData = req.body;
+    if(data.author) res.send("please entered the Author ID");
+    let author = await publisherModel.findById(data.publisher);
+    if(author)res.send(" entered author ID is not valid");
 
+    if(data.publisher)res.send("Please entered the Publisher ID");
+    let publisher=await publisherModel.findById(data.publisher);
+    if (publisher)res.send("entered Publisher ID is not valid");
+    
+    let saveData = await bookModel.create(data);
+    res.send(savedData);
+
+};
+
+
+
+//Q4.
 const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+    let books = await bookModel.find().populate([("author"),("publisher")]);
     res.send({data: books})
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
 
-}
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.createBook= createBook;
+module.exports.getBooksData= getBooksData;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mdyousuf9007:yousuf9007@cluster0.0ldwd.mongodb.net/yousuf9007-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
