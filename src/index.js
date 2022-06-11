029acdfbcc16a7bd9dc459a503051464446f0947
mdyousuf <yousuf9007@gmail.com>
2022-06-11 14:25:04 +0530

    DONE

--- src/index.js
+++ src/index.js
@@ -14,12 +14,18 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use(function(req,res,next){
+    const today=new Date();
+    // console.log(today)
+    let date=today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate();
+    let time=today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    let ip=req.ip;
+    let path=req.path;
+    console.log(date +'\n'+time+'\n'+ip+'\n'+path);
+    next();
+});
+
+
 
 app.use('/', route);
 
