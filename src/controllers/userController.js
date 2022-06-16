5b45aea9db50583834d8baf047b61da34f8b94e1
mdyousuf <yousuf9007@gmail.com>
2022-06-17 00:56:43 +0530

    complete

---
+++ src/controllers/userController.js
@@ -0,0 +1,112 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+const createUser = async function (req, res) {
+  try {
+   let data = req.body;
+   if (Object.keys(data).length != 0) {
+    let savedData = await userModel.create(data)
+     res.status(201).send({ msg: savedData })
+    }
+else res.status(400).send({ msg: "BAD REQUEST" })
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  }
+};
+
+const loginUser = async function (req, res) {
+  let userName = req.body.emailId;
+  let password = req.body.password;
+
+  let user = await userModel.findOne({ emailId: userName, password: password });
+  if (!user)
+    return res.send({
+      status: false,
+      msg: "username or the password is not corerct",
+    });
+  if (user.isDeleted) return res.send("user is deleted already")
+
+  // Once the login is successful, create the jwt token with sign function
+  // Sign function has 2 inputs:
+  // Input 1 is the payload or the object containing data to be set in token
+  // The decision about what data to put in token depends on the business requirement
+  // Input 2 is the secret
+  // The same secret will be used to decode tokens
+  let token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "thorium",
+      organisation: "FUnctionUp",
+    },
+    "functionup-thorium"
+  );
+  res.setHeader("x-auth-token", token);
+  res.send({ status: true, data: token });
+};
+
+const getUserData = async function (req, res) {
+ 
+  let userId = req.params.userId;
+  let userDetails = await userModel.findById(userId);
+  if (!userDetails || userDetails.isDeleted)
+    return res.send({ status: false, msg: "No such user exists" });
+
+  res.send({ status: true, data: userDetails });
+};
+
+const updateUser = async function (req, res) {
+// Do the same steps here:
+// Check if the token is present
+// Check if the token present is a valid token
+// Return a different error message in both these cases
+
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  //Return an error if no user with the given id exists in the db
+  if (!user || user.isDeleted) {
+    return res.send("No such user exists");
+  }
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  res.send({ status: updatedUser, data: updatedUser });
+};
+
+const postMessage = async function (req, res) {
+    let message = req.body.message
+    let user = await userModel.findById(req.params.userId)
+    if(!user || user.isDeleted) return res.send({status: false, msg: 'No such user exists'})
+    let updatedPosts = user.posts
+    //add the message to user's posts
+    updatedPosts.push(message)
+    let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {upsert:true,new: true})
+
+    //return the updated user document
+    return res.send({status: true, data: updatedUser})
+}
+
+const deleteUser =async function(req,res){
+  let userId=req.params.userId;
+  let user =await userModel.findById(userId)
+  console.log(user)
+   //Return an error if no user with the given id exists in the db
+   if (!user) {
+    return res.send("No such user exists");
+  }
+  if(user.isDeleted){
+    return res.send("User data is already Deleted");  
+  }
+  let userDelete=await userModel.findOneAndUpdate({_id:userId},{"isDeleted":true});
+ 
+
+ // let deleteFlag=await userModel.updateOne({_id:userId},{$set:{isDeleted:true}},{upsert:true})
+  res.send({msg:"user successfully deleted",data:userDelete})
+}
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.postMessage = postMessage
+module.exports.deleteUser=deleteUser
